9364f351fe01ea124a82db12ad8ac12e620a6078
Jesse Calvert <jesseclvrt1@gmail.com>
2018-10-22 16:17:34 -0700

    todo: implement operator case in evaluate(char*)

--- calculate.cpp
+++ calculate.cpp
@@ -1,7 +1,60 @@
 #include <iostream>
+#include <stack>
+#include <cmath>
+#include <cstring>
+
+int opOrder(char op) {
+    switch (op) {
+        case('(') :             return 0;
+        case('+') : case('-') : return 1;
+        case('*') : case('/') : return 2;
+        case('^') :             return 3;
+    }
+}
+
+int applyOperation(int x, int y, char op) {
+    switch (op) {
+        case('+') : return x + y;
+        case('-') : return x - y;
+        case('*') : return x * y;
+        case('/') : return x / y;
+        case('^') : return pow(x, y);
+    }
+}
 
 int evaluate(char expression[]) {
-    
+    std::stack<int> values;
+    std::stack<char> operators;
+    for (int i = 0; i < strlen(expression); i++) {
+        if (std::isspace(expression[i])) { continue; }
+
+        else if (std::isdigit(expression[i])) {
+            int value = 0;
+            while (isdigit(expression[i])) {
+                value *= 10;
+                value += expression[i] - '0';
+                i++;
+            }
+            values.push(value);
+        }
+
+        else if (expression[i] == '(') {
+            operators.push(expression[i]);
+        }
+
+        else if (expression[i] == ')') {
+            while (!operators.empty() && operators.top() != '(') {
+                int op2 = values.top();
+                values.pop();
+                int op1 = values.top();
+                values.pop();
+                char op = operators.top();
+                operators.pop();
+                values.push(applyOperation(op1, op2, op));
+            }
+            operators.pop();
+        }
+    }
 }
 
 int main(int argc, char* argv[]) {
